8580b712548e4d859467f49127eeea0df3e205ee
malone <sanxian@sanxiandeMacBook-Pro.local>
2014-05-13 09:40:32 +0800

    Initial Commit

---
+++ TestNavigationControllerPush/SXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SXAppDelegate.h
+//  TestNavigationControllerPush
+//
+//  Created by malone on 14-5-13.
+//  Copyright (c) 2014年 sanxian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestNavigationControllerPush/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestNavigationControllerPush
+//
+//  Created by malone on 14-5-13.
+//  Copyright (c) 2014年 sanxian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SXAppDelegate class]));
+    }
+}
